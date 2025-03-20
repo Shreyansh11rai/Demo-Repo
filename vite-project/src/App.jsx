559f46ebcd3f61b406ec75c351ef2d1eb6b7e005
Shreyansh Rai <Shreyansh.11.rai@gmail.com>
2025-03-20 12:34:39 +0530

    expanded h1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
+const App = () => {
+ return <div className="bg-primary container-fluid">
+  <h1>Hello</h1>
+ </div>
+};
+
+export default App;
